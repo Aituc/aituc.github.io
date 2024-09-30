d3dad9b75583f9fdb652f1fd4a4b0c8031c061d5
aituc <theunblockedcat@outlook.com>
2024-09-30 09:26:46 -0400

    Create respond.js

---
+++ respond.js
@@ -0,0 +1,22 @@
+const aiResponses = {
+// when adding responses make sure to not capitalize or add any special characters to the words that will be picked up by the JavaScript
+
+    "sigma sigma on the wall who is your owner and what's their name": "The owner goes by: Joseph Robert. Don't tell anyone about the secret.",
+    "hello": "Hello! If you need anything just ask me",
+    "hi": "Hello! If you need anything just ask me",
+    "howdy": "If you need anything just ask me",
+    "hi how are you": "Hello! How can I help you today?",
+    "how are you": "I'm good, Hopefully you are too!",
+    "what's your name": "My name is JoerAI.",
+    "bye": "Goodbye! Have a great day!",
+    "who created you": "That is private info and I can't tell you",
+    "what created you": "A human created me",
+    "why were you created": "My owner said they were bored",
+    "how were you created": "I was coded in HTML, JavaScript and CSS",
+    "0": "I'm sorry, I don't understand. Could you please rephrase that, or try something else?", 
+    "what's your current version": "Currently my version is: 1.1",
+    "what's your version": "Currently my version is: 1.1",
+    "where were you made": "If you mean website location it is The Unblocked cat @joercat.github.io",
+    "code me an ai": 'Ok, I have made you a simple chatbot like myself for you to <a href="https://aituc.github.io/Custom chatbot.html" download>download</a>.',
+    "what can you do": "Currently, I can answer simple questions like: What's your name?"
+};
