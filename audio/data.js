cf4328b2b3403b8e2dc50223b7dc08dbb75c9edb
Aituc <theunblockedcat@outlook.com>
2024-10-09 17:12:36 -0400

    Add files via upload

---
+++ audio/data.js
@@ -0,0 +1,51 @@
+const data = [
+    { name: "No Way Back Home", artist: "DJJR", category: "dance", audio: "DJJR - No Way Back Home.mp3" },
+    { name: "No Way Back Home", artist: "DJJR", category: "sad", audio: "DJJR - No Way Back Home.mp3" },
+
+    { name: "Trapped in the Sea", artist: "DJJR", category: "nu-metal", audio: "DJJR - Trapped in the Sea.mp3" },
+    { name: "Trapped in the Sea", artist: "DJJR", category: "numetal", audio: "DJJR - Trapped in the Sea.mp3" },
+    { name: "Trapped in the Sea", artist: "DJJR", category: "nu metal", audio: "DJJR - Trapped in the Sea.mp3" },
+    { name: "Trapped in the Sea", artist: "DJJR", category: "rap", audio: "DJJR - Trapped in the Sea.mp3" },
+    { name: "Trapped in the Sea", artist: "DJJR", category: "rock", audio: "DJJR - Trapped in the Sea.mp3" },
+    { name: "Trapped in the Sea", artist: "DJJR", category: "hard rock", audio: "DJJR - Trapped in the Sea.mp3" },
+
+    { name: "Break the Silence", artist: "DJJR", category: "nu-metal", audio: "DJJR - Break the Silence.mp3" },
+    { name: "Break the Silence", artist: "DJJR", category: "numetal", audio: "DJJR - Break the Silence.mp3" },
+    { name: "Break the Silence", artist: "DJJR", category: "nu metal", audio: "DJJR - Break the Silence.mp3" },
+    { name: "Break the Silence", artist: "DJJR", category: "rap", audio: "DJJR - Break the Silence.mp3" },
+    { name: "Break the Silence", artist: "DJJR", category: "rock", audio: "DJJR - Break the Silence.mp3" },
+    { name: "Break the Silence", artist: "DJJR", category: "hard rock", audio: "DJJR - Break the Silence.mp3" },
+
+    { name: "Can't let go", artist: "DJJR", category: "sad", audio: "DJJR - Can't let go.mp3" },
+    { name: "Can't let go", artist: "DJJR", category: "dance", audio: "DJJR - Can't let go.mp3" },
+
+    { name: "Dancing With Ghosts", artist: "DJJR", category: "sad", audio: "DJJR - Dancing With Ghosts.mp3" },
+    { name: "Dancing With Ghosts", artist: "DJJR", category: "dance", audio: "DJJR - Dancing With Ghosts.mp3" },
+
+    { name: "Reaper's Call", artist: "DJJR", category: "metal", audio: "DJJR - Reaper's Call.mp3" },
+    { name: "Reaper's Call", artist: "DJJR", category: "heavy metal", audio: "DJJR - Reaper's Call.mp3" },
+    { name: "Reaper's Call", artist: "DJJR", category: "death", audio: "DJJR - Reaper's Call.mp3" },
+
+    { name: "Hollow Throne", artist: "DJJR", category: "metal", audio: "DJJR - Hollow Throne.mp3" },
+    { name: "Hollow Throne", artist: "DJJR", category: "heavy metal", audio: "DJJR - Hollow Throne.mp3" },
+    { name: "Hollow Throne", artist: "DJJR", category: "death", audio: "DJJR - Hollow Throne.mp3" },
+
+    { name: "Dancing in the dark", artist: "DJJR", category: "dance", audio: "DJJR - Dancing in the dark.mp3" },
+    { name: "Dancing in the dark", artist: "DJJR", category: "sad", audio: "DJJR - Dancing in the dark.mp3" },
+
+    { name: "Realm of Decay", artist: "DJJR", category: "metal", audio: "DJJR - Realm of Decay.mp3" },
+    { name: "Realm of Decay", artist: "DJJR", category: "heavy metal", audio: "DJJR - Realm of Decay.mp3" },
+    { name: "Realm of Decay", artist: "DJJR", category: "death", audio: "DJJR - Realm of Decay.mp3" },
+
+    { name: "The End Is Near", artist: "DJJR", category: "rock", audio: "DJJR - The End Is Near.mp3" },
+    { name: "The End Is Near", artist: "DJJR", category: "hard rock", audio: "DJJR - The End Is Near.mp3" },
+    { name: "The End Is Near", artist: "DJJR", category: "alternative rock", audio: "DJJR - The End Is Near.mp3" },
+    { name: "The End Is Near", artist: "DJJR", category: "sad", audio: "DJJR - The End Is Near.mp3" },
+
+    { name: "The End Is Near", artist: "DJJR", category: "rock", audio: "DJJR - Fading Flame.mp3" },
+    { name: "The End Is Near", artist: "DJJR", category: "hard rock", audio: "DJJR - Fading Flame.mp3" },
+    { name: "The End Is Near", artist: "DJJR", category: "alternative rock", audio: "DJJR - Fading Flame.mp3" },
+    { name: "The End Is Near", artist: "DJJR", category: "sad", audio: "DJJR - Fading Flame.mp3" },
+
+    { name: "The End Is Near", artist: "DJJR", category: "Q", audio: "DJJR - The End Is Near.mp3" },
+];
